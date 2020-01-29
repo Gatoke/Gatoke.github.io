6a026b5c0c1b301793168f45bdcf0e4e6003addf
Karol Dominiak <k.dominiak@samsung.com>
2020-01-29 12:54:27 +0100

    some animations

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(function() {
+
+    // enable tooltips
+    $("body").tooltip({ selector: '[data-toggle=tooltip]'});
+
+
+
     // Sidebar toggle behavior
     $('#sidebarCollapse').on('click', function() {
         $('#sidebar, #content').toggleClass('active');
